870ce6cac7887e6160410b38c39ec868320af42b
Jose Herminio Collas <goatstone@gmail.com>
2024-06-02 19:03:23 +0000

    Add Combobox to main page. Add pages for other components.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,29 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
-import { FluentProvider, webLightTheme, Button } from "@fluentui/react-components";
+import {
+  FluentProvider,
+  webLightTheme,
+  Button,
+} from "@fluentui/react-components";
 import { Multiselect } from "../Multiselect";
+import {
+  Combobox,
+  ComboboxProps,
+  makeStyles,
+  useComboboxFilter,
+  useId,
+} from "@fluentui/react-components";
 
+const useStyles = makeStyles({
+  root: {
+    // Stack the label above the field with a gap
+    display: "grid",
+    gridTemplateRows: "repeat(1fr)",
+    justifyItems: "start",
+    gap: "2px",
+    maxWidth: "400px",
+  },
+});
 const pageStyles = {
   color: "#232129",
   padding: 96,
@@ -16,144 +37,66 @@ const headingStyles = {
 const headingAccentStyles = {
   color: "#663399",
 };
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-};
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-};
-const doclistStyles = {
-  paddingLeft: 0,
-};
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-};
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-};
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-};
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-};
-
-const docLinks = [
+const options = [
+  { children: "Alligator", value: "ec5a9b9e-af83-4bf2-be3d-860583a031cb" },
+  { children: "Bee", value: "f2245404-59a7-44e7-842b-df63d6b4741b" },
+  { children: "Bird", value: "0b9c4ffd-b894-4c89-bb81-ef8c0e0bf6d4" },
   {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
+    children: "Cheetah",
+    disabled: true,
+    value: "ed16f197-94e9-47f1-adcb-cf182dc4a171",
   },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
-  },
-];
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-};
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
+  { children: "Dog", value: "09acb1ce-967e-4fb2-b68e-e17b4e15082e" },
+  { children: "Dolphin", value: "3c40f518-df7d-403d-a8c7-ac8f610b66ca" },
+  { children: "Ferret", value: "378c8661-d86b-4518-8adb-64009625c35b" },
+  { children: "Firefly", value: "5d5edfa2-f9ba-4044-a142-33d957cd6d7d" },
+  { children: "Fish", value: "e2b16f19-5934-4e4d-9b6b-bc5235750829" },
+  { children: "Goat", value: "2314defd-49ec-4576-aed3-4e639ba65e08" },
+  { children: "Horse", value: "bea41f2a-127c-4519-9366-01e2d2ae4f8a" },
+  { children: "Lion", value: "b7e394bf-1066-49c0-bf17-ba0deb287dea" },
 ];
 
 const IndexPage: React.FC<PageProps> = () => {
+  const comboId = useId();
+  const styles = useStyles();
+  const [query, setQuery] = React.useState<string>("");
+  const onOptionSelect: ComboboxProps["onOptionSelect"] = (e, data) => {
+    setQuery(data.optionText ?? "");
+  };
+  function optionToText(
+    option: string | { children: React.ReactNode; value: string },
+  ) {
+    if (typeof option === "string") {
+      return option;
+    }
+    if (Array.isArray(option.children)) {
+      return option.children
+        .map((optionElement) => optionElement.toString())
+        .toString();
+    }
+    if (option.children) {
+      return option.children.toString();
+    }
+    return "";
+  }
+  const children = useComboboxFilter(query, options, {
+    noOptionsMessage: "No animals match your search.",
+    optionToText,
+  });
+
   return (
     <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        React Fluent 9 Examples
-        <br />
-        <span style={headingAccentStyles}>
-          XXXxxx
-        </span>
-      </h1>
+      <h1 style={headingStyles}>React Fluent 9</h1>
       <FluentProvider theme={webLightTheme}>
-        <Multiselect />
-        <span style={headingAccentStyles}>
-          XXX
-        </span>
-        <Button onClick={e=>console.log(e)}>xxx</Button>
+        <Combobox
+          onOptionSelect={onOptionSelect}
+          aria-labelledby={comboId}
+          placeholder="Select an animal"
+          onChange={(ev) => setQuery(ev.target.value)}
+          value={query}
+        >
+          {children}
+        </Combobox>
       </FluentProvider>
     </main>
   );
